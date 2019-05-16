aafca181845376024710b3923583764dda6b1ba5
Benjamin Vincent <luxcium@neb401.com>
2019-05-16 04:46:02 -0400

    will publish

--- src/utils/QuestradeHelperFunction.ts
+++ src/utils/QuestradeHelperFunction.ts
@@ -5,22 +5,18 @@ export async function QuestradeHelperFunction(
   opts: IQuestradeAPIOptions,
   cb?: (qt: QuestradeClass) => Promise<QuestradeClass>
 ) {
-  // tslint:disable-next-line: max-line-length
-  //  Promise<QuestradeClass> (value?: QuestradeClass |  Promise<QuestradeClass> | undefined) => void
-  return new Promise((
-    resolve: (qt: QuestradeClass) => any /*  Promise<QuestradeClass> */,
-    _reject: (error: Error) => any
-  ) => {
-    const qt = new QuestradeClass(opts);
-    qt.on('ready', async () => {
-      if (typeof cb === 'function') {
-        console.log('ready', 'ready');
-        return resolve(await cb(qt));
-      }
-      return resolve(qt);
-    });
-    // reject(new Error('cb is not a functions callback'));
-  });
+  return new Promise(
+    (resolve: (qt: QuestradeClass) => any, _reject: (error: Error) => any) => {
+      const qt = new QuestradeClass(opts);
+      qt.on('ready', async () => {
+        if (typeof cb === 'function') {
+          console.log('ready', 'ready');
+          return resolve(await cb(qt));
+        }
+        return resolve(qt);
+      });
+    }
+  );
 }
 export const tokenConnection = async (token: string) => {
   const questrade = await QuestradeHelperFunction({ seedToken: token });
