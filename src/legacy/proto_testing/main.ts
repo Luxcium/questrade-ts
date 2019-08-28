80b466c502d685ad4f3ce3e5924c913f19774fde
Benjamin Vincent <luxcium@neb401.com>
2019-08-27 22:07:17 -0400

    delete proto testing
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/legacy/proto_testing/main.ts
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-import { main } from './index';
-// will void this token bfore publishing do not use this token
-const seedToken = '8gVXfsNeGezpqbnRYF5jrgxwBLFFjABb0';
-main(seedToken).catch(error =>
-  console.log('MAINERROR in PROTO TESTING:', error)
-);
