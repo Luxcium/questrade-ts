402cb09e041a2b4082ed24f07ed7eb8f0581a8f0
Benjamin Vincent <luxcium@neb401.com>
2019-09-10 05:41:42 -0400

    autentication logic
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

---
+++ src/core/secure/index.ts
@@ -0,0 +1,9 @@
+export { apiGet } from './apiGet';
+export {
+  getAccessToken,
+  oAuthLogic,
+  validateAuthOptions,
+} from './getAccessToken';
+
+// validateAuthOptions(this, options)
+// oAuthLogic(this).then
