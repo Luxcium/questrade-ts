32b5da6456d912affd26dbdbf480eb75a6c21365
Benjamin Vincent <luxcium@neb401.com>
2019-09-11 01:36:55 -0400

    moving function in own file getTimeFirst
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

---
+++ src/core/secure/_getTimeFirst.ts
@@ -0,0 +1,28 @@
+import { Credentials } from '../libraries/Credentials';
+import { QuestradeAPIOptions } from '../types';
+import { _apiGet } from './_apiGet';
+import { _oAuthLogic } from './_oAuthLogic';
+/** As first test will get time from server after oAuthentiocation  */
+export async function _getTimeFirst(
+  options: QuestradeAPIOptions,
+  cb: any = (creds: Credentials) => creds
+) {
+  const credentials = await _oAuthLogic(options);
+  try {
+    const timeFirst = await _getTime(credentials);
+    console.log('TIME getTimeFirst', timeFirst);
+    // apiGet('',theSelf.accessToken)
+  } catch (error) {
+    //
+  }
+  return cb(credentials);
+}
+
+export const _getTime = async (credentials: Credentials) => {
+  try {
+    const { time } = await _apiGet('/time', credentials);
+    return time;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
