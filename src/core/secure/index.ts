be9289e11d474744f9414131c5fb2d9d00486c96
Benjamin Vincent <luxcium@neb401.com>
2019-09-11 01:33:50 -0400

    refactoring the api logic for oauth and apiget
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/core/secure/index.ts
+++ src/core/secure/index.ts
@@ -1,63 +1,61 @@
 import { Credentials } from '../libraries/Credentials';
 import { QuestradeAPIOptions } from '../types';
 import { _apiGet } from './_apiGet';
-import {
-  // getAccessToken,
-  oAuthLogic, validateAuthOptions
-} from './_getAccessToken';
+import { _oAuthLogic } from './_oAuthLogic';
 
 /** As first test will get time from server after oAuthentiocation  */
 export async function _getTimeFirst(
   options: QuestradeAPIOptions,
-  cb: any = () => void 0
+  cb: any = (creds: Credentials) => creds
 ) {
-  const credentials = validateAuthOptions(options);
+  const credentials = await _oAuthLogic(options);
 
   try {
-    const oAuth = await oAuthLogic(credentials);
-    const timeFirst = await _getTime(oAuth);
+    const timeFirst = await _getTime(credentials);
     console.log('TIME getTimeFirst', timeFirst);
     // apiGet('',theSelf.accessToken)
   } catch (error) {
     //
   }
-  oAuthLogic(credentials)
-    .then((self: any) => {
-      self
-        // getTime()
-        .then((time: any) => {
-          console.info('Server Time:', new Date(time).toLocaleString());
-          console.info(
-            'self',
-            self.tokenType,
-            'token expire in',
-            self.expiresIn / 60,
-            'minutes'
-          );
-          self.emit('ready', self);
-        })
-        .catch((err: Error) => {
-          console.error(err);
-          try {
-            // credentials.emit('error', 'Can not get server time', err);
-          } catch (error) {
-            console.error('Canot get server time');
-          }
-        });
-    })
-    .catch(_callingMainError => {
-      console.error(
-        'Error calling main() from QuestradeClient class in constructor'
-      );
-    });
   return cb(credentials);
 }
+// _oAuthLogic(credentials)
+//   .then((self: any) => {
+//     self
+//       // getTime()
+//       .then((time: any) => {
+//         console.info('Server Time:', new Date(time).toLocaleString());
+//         console.info(
+//           'self',
+//           self.tokenType,
+//           'token expire in',
+//           self.expiresIn / 60,
+//           'minutes'
+//         );
+//         self.emit('ready', self);
+//       })
+//       .catch((err: Error) => {
+//         console.error(err);
+//         try {
+//           // credentials.emit('error', 'Can not get server time', err);
+//         } catch (error) {
+//           console.error('Canot get server time');
+//         }
+//       });
+//   })
+//   .catch(_callingMainError => {
+//     console.error(
+//       'Error calling main() from QuestradeClient class in constructor'
+//     );
+//   });
 
-const _getTime = async (oAuth: Credentials) => {
+const _getTime = async (credentials: Credentials) => {
   try {
-    const { time } = await _apiGet('/time', oAuth); // this._api<Time>('/time');
+    const { time } = await _apiGet('/time', credentials);
     return time;
   } catch (error) {
     throw new Error(error.message);
   }
 };
+
+export const questradeAPI = [_oAuthLogic, _apiGet];
