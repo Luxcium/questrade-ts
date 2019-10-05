54d54b6125e84288d2154694de02e36e16427670
Benjamin Vincent <luxcium@neb401.com>
2019-10-04 22:24:56 -0400

    alowed export out of the package
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import * as utils from './utils';
 export { qtEnumerations as Enumerations } from 'questrade-api-enumerations';
 export { redeemToken } from './api';
 export { testEnumerations } from './test';
 export { day } from './utils';
 export { utils };
-import * as utils from './utils';
