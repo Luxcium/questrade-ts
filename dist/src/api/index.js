1a76e2fb75355fbf61690609ee366263260da55b
Benjamin Vincent <luxcium@neb401.com>
2019-10-10 08:59:43 -0400

    getting ready for next version
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- dist/src/api/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var public_1 = require("./public");
-exports.redeemToken = public_1.redeemToken;
-//# sourceMappingURL=index.js.map
--- dist/src/api/private/core/AxiosRequestApiFactory/_endpointFormatAccount/_endpointFormatAccount.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/** PROVIDE: credentials and accountEndpoint string, THEN GET: a endpoint string */
-exports._endpointFormatAccount = (credentials) => (accountEndpoint) => `/accounts/${credentials.accountNumber}${accountEndpoint}`;
-//# sourceMappingURL=_endpointFormatAccount.js.map
