29486559f49246074fe7ef82c9cfb31caf6088c8
Benjamin Vincent <luxcium@neb401.com>
2019-10-10 10:58:39 -0400

    will dist
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/api/public/credentialsFactory.ts
+++ src/api/public/credentialsFactory.ts
@@ -2,7 +2,7 @@ import { AxiosStatic, default as axios } from 'axios';
 import { _credentialsFactory } from '../private';
 import { questradeApi } from './questradeAPI';
 
-export const redeemToken = (_axios: AxiosStatic = axios) => async (
+const _redeemToken = (_axios: AxiosStatic = axios) => async (
   refreshToken: string
 ) => {
   const credentials = await _credentialsFactory(_axios)(refreshToken);
@@ -10,3 +10,6 @@ export const redeemToken = (_axios: AxiosStatic = axios) => async (
   const qtApi = questrade;
   return { qtApi, credentials };
 };
+
+const redeemToken = _redeemToken(axios);
+export { redeemToken };
