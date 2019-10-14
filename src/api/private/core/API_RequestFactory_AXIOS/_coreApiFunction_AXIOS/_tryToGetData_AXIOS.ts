9b552e9edc8bea2b409e288abc245c002e1f0153
Benjamin Vincent <luxcium@neb401.com>
2019-10-14 17:39:46 -0400

    renamed folder Axios API Factory
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/api/private/core/API_RequestFactory_AXIOS/_coreApiFunction_AXIOS/_tryToGetData_AXIOS.ts
+++ src/api/private/core/API_RequestFactory_AXIOS/_coreApiFunction_AXIOS/_tryToGetData_AXIOS.ts
@@ -7,7 +7,11 @@ export const _tryToGetData = <R, D>(_axios: AxiosStatic = axios) => {
     return async (_logError: LogErrors = logErrors): Promise<R> => {
       try {
         const data: void | R = await _axios(_config)
-          .then((res: AxiosResponse<R>) => res.data)
+          .then((res: AxiosResponse<R>) => {
+            const rData = res.data;
+            console.log(rData);
+            return rData;
+          })
           .catch(error => console.log(error));
         if (!data) {
           throw _logError(new Error("Can't retrive data from call to API"));
