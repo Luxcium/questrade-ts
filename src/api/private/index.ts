9dc03bda96805cfd32bb96c329b6444f1e7c6c4b
Benjamin Vincent <luxcium@neb401.com>
2019-10-17 20:37:20 -0400

    foldr name change
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/api/private/index.ts
+++ src/api/private/index.ts
@@ -1,7 +1,7 @@
 export { _getQuestradeApi } from './api/QuestradeApi_QtApi';
-export { _axiosAccountGetApi, _axiosGetApi, _axiosPostApi } from './core';
 export {
   _credentialsFactory,
   _emptyCredentials,
   _getPrimaryAccountNumber,
-} from './security';
+} from './authentication';
+export { _axiosAccountGetApi, _axiosGetApi, _axiosPostApi } from './core';
