99f26da2bf0c3f73320249d13cfa551c7caacfba
Benjamin Vincent <luxcium@neb401.com>
2019-10-17 21:33:09 -0400

    questrade api  credentials auth
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/core/public/questradeAPI.ts
+++ src/core/public/questradeAPI.ts
@@ -5,34 +5,6 @@ import { Credentials, IQuestradeApi } from '../typescript';
 
 export const questradeApi = async (credentials: Credentials) => {
   const qtApi = await _getQuestradeApi(credentials);
-  // const currentAccount = qtApi.currentAccount;
-  // const myBalances = qtApi.myBalances;
-  // const serverTime = qtApi.serverTime;
-
-  // const accountActivities = qtApi.get.account.activities;
-  // const accountAllAccounts = qtApi.get.account.allAccounts;
-  // const accountAllOrders = qtApi.get.account.allOrders;
-  // const accountBalances = qtApi.get.account.balances;
-  // const accountExecutions = qtApi.get.account.executions;
-  // const accountOrders = qtApi.get.account.orders;
-  // const accountOrdersByIds = qtApi.get.account.ordersByIds;
-  // const accountPositions = qtApi.get.account.positions;
-
-  // const marketAllMarkets = qtApi.get.market.allMarkets;
-  // const marketCandlesByStockId = qtApi.get.market.candlesByStockId;
-
-  // const quotesByStockIds = qtApi.get.quotes.byStockIds;
-  // const quotesByStrategies = qtApi.get.quotes.byStrategies;
-  // const quotesByOptionsIds = qtApi.get.quotes.optionsQuotes.byOptionsIds;
-  // const quotesFromFilter = qtApi.get.quotes.optionsQuotes.fromFilter;
-
-  // const symbolsOptionChainsByStockId = qtApi.get.symbols.optionChains.byStockId;
-  // const symbolsByStockIds = qtApi.get.symbols.byStockIds;
-
-  // const searchAllStocks = qtApi.get.search.allStocks;
-  // const searchStock = qtApi.get.search.stock;
-  // const searchCountResults = qtApi.get.search.countResults;
-
   const api: IQuestradeApi = {
     currentAccount: qtApi.currentAccount,
     myBalances: qtApi.myBalances,
