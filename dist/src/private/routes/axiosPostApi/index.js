9eed295b333d02587c33bf617ba79ec3e39eab41
Benjamin Vincent <luxcium@neb401.com>
2019-10-19 05:47:18 -0400

    merging from origin next/version into local master

--- dist/src/private/routes/axiosPostApi/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var _axiosPostApi_1 = require("./_axiosPostApi");
-exports._axiosPostApi = _axiosPostApi_1._axiosPostApi;
-//# sourceMappingURL=index.js.map
