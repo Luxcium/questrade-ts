43dcfdc367c9a9a7195c6eb50ca08d7cd3d5adb5
Benjamin Vincent <luxcium@neb401.com>
2019-10-19 13:50:47 -0400

    getting ready to build next/version
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- dist/src/private/core/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var _coreApiFunction_1 = require("./_coreApiFunction");
-exports._coreApiFunction = _coreApiFunction_1._coreApiFunction;
-//# sourceMappingURL=index.js.map
--- dist/src/private/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var axiosAccountGetApi_1 = require("./axiosAccountGetApi");
-exports._axiosAccountGetApi = axiosAccountGetApi_1._axiosAccountGetApi;
-var axiosGetApi_1 = require("./axiosGetApi");
-exports._axiosGetApi = axiosGetApi_1._axiosGetApi;
-var axiosPostApi_1 = require("./axiosPostApi");
-exports._axiosPostApi = axiosPostApi_1._axiosPostApi;
-//# sourceMappingURL=index.js.map
