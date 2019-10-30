8397fe06253dc3c675b302582a97a161d88c6495
Benjamin Vincent <luxcium@neb401.com>
2019-10-30 18:24:15 -0400

    ready for next version

--- dist/src/private/auth/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var credentialsFactory_1 = require("./credentialsFactory");
-exports._credentialsFactory = credentialsFactory_1._credentialsFactory;
-exports._emptyCredentials = credentialsFactory_1._emptyCredentials;
-exports._getPrimaryAccountNumber = credentialsFactory_1._getPrimaryAccountNumber;
-//# sourceMappingURL=index.js.map
