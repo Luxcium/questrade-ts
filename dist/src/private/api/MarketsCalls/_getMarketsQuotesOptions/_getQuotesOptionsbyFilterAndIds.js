abb4eb612c03d0e728bdb38dbeb8e5e1fab7c351
Benjamin Vincent <luxcium@neb401.com>
2019-11-01 00:47:08 -0400

    ready to work on next version

--- dist/src/private/api/MarketsCalls/_getMarketsQuotesOptions/_getQuotesOptionsbyFilterAndIds.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MarketsCalls_1 = require("../../MarketsCalls");
-exports._getQuotesOptionsbyFilterAndIds = function (credentials) {
-    var quotesOptionsFilter = MarketsCalls_1._getQuotesOptionsFilter(credentials);
-    quotesOptionsFilter.byIds = MarketsCalls_1._getQuotesOptionsByIds(credentials);
-    return quotesOptionsFilter;
-};
-//# sourceMappingURL=_getQuotesOptionsbyFilterAndIds.js.map
