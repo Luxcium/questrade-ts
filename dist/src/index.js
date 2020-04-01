f10cc4874a480ee588be5cf68d39a6db6ea98be0
Benjamin Vincent <luxcium@neb401.com>
2020-04-01 11:36:04 -0400

    ready to next as of 20200401
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var public_1 = require("./public");
-exports.redeemToken = public_1.redeemToken;
-exports.introspect = { onOff: false };
-//# sourceMappingURL=index.js.map
