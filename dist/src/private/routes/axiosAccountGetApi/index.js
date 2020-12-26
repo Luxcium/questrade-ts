0bb5968271c106361753a670eaadd878294740db
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-12-26 14:27:31 -0500

    remove old files and tslint to start next branch
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- dist/src/private/routes/axiosAccountGetApi/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var _axiosAccountGetApi_1 = require("./_axiosAccountGetApi");
-exports._axiosAccountGetApi = _axiosAccountGetApi_1._axiosAccountGetApi;
-//# sourceMappingURL=index.js.map
--- dist/src/typescript/ITimeRateLimiter.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ITimeRateLimiter.js.map
