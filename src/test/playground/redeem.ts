b651a5e41dd32f4bf281e0afecbafbe269209708
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-12-31 15:08:29 -0500

    ERROR:can not get the account number from request
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/test/playground/redeem.ts
+++ src/test/playground/redeem.ts
@@ -4,21 +4,22 @@ import { getMyToken } from '../../get-token';
 
 // inside of an async function or async IIFE
 (async () => {
-  const log = console.log;
+  const echo = console.log;
 
   const { qtApi, credentials } = await redeemToken(getMyToken());
 
   // Validate the server time as your hello world for this package
-  const serverTime = qtApi.serverTime;
-  log(serverTime);
+  // const serverTime = qtApi.serverTime;
+  // echo(serverTime);
   // inside an async function use await qt.get.<... some properties or methods>
   const myBalances = await qtApi.myBalances();
   const balances = await qtApi.account.getBalances();
 
-  log(myBalances);
-  log(balances);
-
-  log(credentials);
-
+  echo(myBalances);
+  echo(balances);
+  echo(await qtApi.account.getAllAccounts());
+  void credentials;
+  // echo(credentials);
   // you can use a try/catch block to manage error instead:
+  echo('hello as a test');
 })().catch(error => console.error(error.message));
