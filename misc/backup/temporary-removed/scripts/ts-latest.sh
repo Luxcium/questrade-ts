e4cf375cd6e7dfb93f1abac9006dbcbd3ee938d2
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-01-06 10:56:24 -0500

    update tsc typescript version to stable
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- misc/backup/temporary-removed/scripts/ts-latest.sh
+++ misc/backup/temporary-removed/scripts/ts-latest.sh
@@ -3,7 +3,16 @@
 # On my local computer I am using zsh you may have to change it to bash or sh.
 
 code --uninstall-extension ms-vscode.vscode-typescript-next
-yarn add typescript@latest
+fnm default 14
+fnm use 14
+yarn add typescript@latest @types/node@14.14.20
 npm -g i typescript@latest
 yarn global add typescript@latest
+npm install -g npm@latest
+yarn global add yarn@latest
+yarn global add npm@latest
+tsc --version
+node --version
+npm --version
+tsc
 exit 0
--- misc/backup/temporary-removed/scripts/ts-next.sh
+++ misc/backup/temporary-removed/scripts/ts-next.sh
@@ -3,7 +3,16 @@
 # On my local computer I am using zsh you may have to change it to bash or sh.
 
 code --install-extension ms-vscode.vscode-typescript-next
-yarn add typescript@next
+fnm default 15
+fnm use default
+yarn add -D typescript@next @types/node@14.14.20
 npm -g i typescript@next
 yarn global add typescript@next
+npm install -g npm@7
+yarn global add yarn@latest
+yarn global add npm@7
+tsc --version
+node --version
+npm --version
+tsc
 exit 0
