f30275f0ea0545082cbd4b7c0099f5e6a5e73b90
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-02-16 08:16:31 -0500

    export const qtAPIv2_0
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export { questradeAPI } from './public';
+import { questradeAPI } from './public';
+import { IQuestradeAPIv2_0 } from './public/IQuestradeAPI';
+import { ApiOptions, Credentials } from './typescript';
+
+export const qtAPIv2_0: (
+  apiOptions: ApiOptions,
+) => Promise<{
+  credentials: Credentials;
+  qtApi: IQuestradeAPIv2_0;
+}> = questradeAPI;
