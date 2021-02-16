23840b5d8713ae29ce13384acf1e1298c846f72f
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-02-16 06:57:35 -0500

    no-useless-constructor in typescript #14118
    https://github.com/eslint/eslint/issues/14118
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/resources/side-effects/proxies/core/reflexion-logger-proxy-handler-abstarct-class.ts
+++ src/resources/side-effects/proxies/core/reflexion-logger-proxy-handler-abstarct-class.ts
@@ -6,9 +6,7 @@ import { ProxyHandlerOptions } from '../../types';
 export abstract class ReflexionLoggerProxyHandlerAbstractClass<
   T extends Function = any
 > implements ProxyHandler<T> {
-  constructor(protected handlerOptions: ProxyHandlerOptions) {
-    //
-  }
+  constructor(protected handlerOptions: ProxyHandlerOptions) {}
 
   getPrototypeOf(target: T): object | null {
     if (this.handlerOptions?.debuging === true) {
