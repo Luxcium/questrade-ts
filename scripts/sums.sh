48885c58d11ff9374a2d2e03ebb5ad6135ecb1ea
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-02-16 10:09:04 -0500

    add sum script
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

---
+++ scripts/sums.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+md5sum ./src/public/IQuestradeAPIv2_0.ts
+sha1sum ./src/public/IQuestradeAPIv2_0.ts
+sha256sum ./src/public/IQuestradeAPIv2_0.ts
+sha512sum ./src/public/IQuestradeAPIv2_0.ts
