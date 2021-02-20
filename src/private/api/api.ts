89c6a0fc67cdc6ff4f02c43ae607ca8c8bc3cf0d
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-02-20 03:58:19 -0500

    changes made
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

---
+++ src/private/api/api.ts
@@ -0,0 +1,104 @@
+import type { IAccountActivity, IActivities } from '../../typescript';
+
+export const API_TS = 'api.ts';
+
+// GET accounts/:id/activities
+export type { IAccountActivity, IActivities };
+/*
+// GET accounts/:id/activities
+export interface IActivities {
+  activities: IAccountActivity[];
+}
+export interface IAccountActivity {
+  tradeDate: Date | string;
+  transactionDate: Date | string;
+  settlementDate: Date | string;
+  action: string;
+  symbol: string;
+  // /** symbol ID *-/
+  stockId: string | number;
+  // /** description *-/
+  description: string;
+  // /** enumeration Currency *-/
+  currency: Currency;
+  // /** the quantity *-/
+  quantity: number;
+  // /** the price *-/
+  price: number;
+  // /** gross amount *-/
+  grossAmount: number;
+  // /** the commission *-/
+  commission: number;
+  // /** net Amount *-/
+  netAmount: number;
+  // /** activity Type *-/
+  type: string;
+}
+
+GET accounts/:id/orders[/:orderId]
+export interface IOrders {
+  orders: IOrder[];
+}
+export interface IOrder {
+  id?: string | number;
+  symbol?: string;
+  stockId?: string | number;
+  totalQuantity?: number;
+  openQuantity?: number;
+  filledQuantity?: number;
+  canceledQuantity?: number;
+  side?: OrderSide;
+  orderType?: OrderType;
+  limitPrice?: number | null;
+  stopPrice?: number | null;
+  isAllOrNone?: boolean;
+  isAnonymous?: boolean;
+  icebergQuantity?: number | null;
+  minQuantity?: number | null;
+  avgExecPrice?: number | null;
+  lastExecPrice?: number | null;
+  source?: string;
+  timeInForce?: OrderTimeInForce;
+  gtdDate?: Date | string | null;
+  state?: OrderState;
+  clientReasonStr?: string;
+  chainId?: string | number;
+  creationTime?: Date | string;
+  updateTime?: Date | string;
+  notes?: string;
+  primaryRoute?: string;
+  secondaryRoute?: string;
+  orderRoute?: string;
+  venueHoldingOrder?: string;
+  commissionCharged?: number;
+  exchangeOrderId?: string | number | string;
+  isSignificantShareHolder?: boolean;
+  isInsider?: boolean;
+  isLimitOffsetInDollar?: boolean;
+  userId?: string | number;
+  placementCommission?: number | null;
+  legs?: [];
+  OrderLeg?: string;
+  strategyType?: StrategyTypes | 'SingleLeg';
+  triggerStopPrice?: number | null;
+  orderGroupId?: string | number;
+  orderClass?: OrderClass | null;
+  rejectionReason?: string;
+  comissionCharged?: number;
+  isCrossZero?: boolean;
+}
+
+GET accounts/:id/executions
+GET accounts/:id/balances
+GET accounts/:id/positions
+GET accounts/:id/positions
+GET time
+GET markets/candles/:id
+GET markets/quotes/strategies
+GET markets/quotes/options
+GET markets/quotes/:id
+GET markets
+GET symbols/:id/options
+GET symbols/search
+GET symbols/:id
+*/
---
+++ src/test/test.spec.ts
@@ -0,0 +1,3 @@
+import { qtAPIv2_0 } from '..';
+
+qtAPIv2_0;
