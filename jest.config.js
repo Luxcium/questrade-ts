5e0f47023dbee4f3ad40418c11ca8a736f705cf8
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-02-24 06:28:34 -0500

    change to the scripts and testing profile
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

---
+++ jest.config.js
@@ -0,0 +1,103 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  'preset': 'ts-jest',
+  'testEnvironment': 'node',
+  'globals': {
+    'ts-jest': {
+      diagnostics: true,
+    },
+  },
+
+  'moduleFileExtensions': ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+
+  'verbose': true,
+
+  //
+  'automock': true,
+  'bail': false,
+  'cacheDirectory': './.cache/jest',
+  'clearMocks': false,
+  'collectCoverage': true,
+  'collectCoverageFrom': ['src/**/*.ts'],
+  'coverageDirectory': './coverage',
+  // coveragePathIgnorePatterns: [],
+  // // coverageProvider: '',
+  'coverageReporters': ['json', 'lcov', 'text', 'clover'],
+  'coverageThreshold': {
+    'global': {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+    './src/utils/**/*.ts': {
+      branches: 40,
+      statements: 40,
+    },
+    './src/reducers/**/*.ts': {
+      statements: 90,
+    },
+    './src/api/**/*.ts': {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  // dependencyExtractor: '',
+  // displayName: [],
+  // errorOnDeprecated: true,
+  // extraGlobals: [],
+  // forceCoverageMatch: [],
+  // globals: {},
+  // globalSetup: '',
+  // globalTeardown: '',
+  // haste: {},
+  // injectGlobals: true, // false
+  // maxConcurrency: 0,
+  // moduleDirectories: [], // [] ['array<string>'],
+  // moduleFileExtensions: [], // [] ['array<string>'],
+  // moduleNameMapper: [], // [] ["object<string, string | 'array<string>>'"],
+  // modulePathIgnorePatterns: [], // [] ['array<string>'],
+  // modulePaths: [], // [] ['array<string>'],
+  // notify: true, // false
+  // notifyMode: '',
+  // preset: '[string]',
+  // prettierPath: '[string]',
+  // projects: [], // [] ['array<string | ProjectConfig>'],
+  // reporters: [], // [] ['array<moduleName | [moduleName, options]>'],
+  // resetMocks: true, // false
+  // resetModules: true, // false
+  // resolver: '[string]',
+  // restoreMocks: true, // false
+  // rootDir: '[string]',
+  // roots: [], // [] ['array<string>'],
+  // runner: '[string]',
+  // setupFiles: [], // [] ['array'],
+  // setupFilesAfterEnv: [], // [] ['array'],
+  // slowTestThreshold: 0,
+  // snapshotResolver: '[string]',
+  // snapshotSerializers: [], // [] ['array<string>'],
+  // testEnvironment: '[string]',
+  // testEnvironmentOptions: {},
+  // testFailureExitCode: 0,
+  // testMatch: [], // [] ['array<string>'],
+  // testPathIgnorePatterns: [], // [] ['array<string>'],
+  // testRegex: [], // [] ["string | 'array<string>'"],
+  // testResultsProcessor: '[string]',
+  // testRunner: '[string]',
+  // testSequencer: '[string]',
+  // testTimeout: 0,
+  // testURL: '[string]',
+  // timers: '[string]',
+  // transform: [], // [] [
+  // // 'object<string, pathToTransformer | [pathToTransformer, object]>',
+  // // ],
+  // transformIgnorePatterns: [], // [] ['array<string>'],
+  // unmockedModulePathPatterns: [], // [] ['array<string>'],
+  // watchPathIgnorePatterns: [], // [] ['array<string>'],
+  // watchPlugins: [], // [] ['array<string | [string, Object]>'],
+  'watchman': true,
+  '//': 'Comment goes here',
+  // //  */
+};
---
+++ scripts/pre-publish.js
@@ -0,0 +1,94 @@
+const { readFile, writeFile } = require('fs');
+const { promisify } = require('util');
+
+async function prePublish() {
+  const filePath = './package.json';
+  const writePath = './dist/package.json';
+
+  const name = 'questrade-ts';
+  const displayName = 'Questrade API by Luxcium (with typescript support).';
+  const description =
+    'Unofficial Questrade API wrapper for NodeJS with full TypeScript support.';
+  const author = 'Luxcium <luxcium@neb401.com>';
+  const license = 'MIT';
+  const homepage = 'https://github.com/Luxcium/questrade-ts';
+  const main = './index.js';
+  const types = './typings/index.d.ts';
+
+  const readFileAsync = promisify(readFile);
+  const writeFileAsync = promisify(writeFile);
+  const rawdata = await readFileAsync(filePath);
+  const package = JSON.parse(rawdata);
+  const { scripts, devDependencies, version, ...partialPackage } = package;
+
+  void partialPackage;
+  const nowDate = new Date();
+  // const date = ;
+  const newPackage = {
+    name,
+    displayName,
+    description,
+    version: `${version}+${nowDate.getFullYear()}${
+      nowDate.getMonth() + 1 > 9
+        ? nowDate.getMonth() + 1
+        : '0' + (nowDate.getMonth() + 1)
+    }${nowDate.getDate()}v2.0c`,
+    author,
+    license,
+    homepage,
+    main,
+    types,
+    ...partialPackage,
+  };
+  writeFileAsync(writePath, JSON.stringify(newPackage));
+  // console.dir(newPackage);
+  // return returnValue;
+}
+
+prePublish();
+module.exports = { prePublish };
+
+process.on('uncaughtException', error =>
+  error.code === 'ERR_INVALID_CALLBACK'
+    ? console.warn({ errorCode: error.code }) || process.exit(2)
+    : console.error({ uncaughtException: error }) || process.exit(1),
+);
+
+//   repository: {
+//     type: 'git',
+//     url: 'git@github.com/luxcium/questrade-ts.git',
+//   },
+//   bugs: { url: 'https://github.com/luxcium/questrade-ts/issues' },
+//   keywords: [
+//     'TypeScript',
+//     'Canadian',
+//     'Stock Market API',
+//     'Un-Official',
+//     'Questrade',
+//     'Canada',
+//     'API Wrapper',
+//     'Questrade API',
+//     'Stocks',
+//     'Trading',
+//     'Luxcium',
+//     'TypeScript Support',
+//     'API wrapper',
+//     'TypeScript support',
+//     'stock market api',
+//     'canada',
+//     'Canadian',
+//   ],
+//   peerDependencies: { axios: '^0.21.1' },
+//   optionalDependencies: {
+//     '@types/ioredis': '^4.19.2',
+//     '@types/node': '^14.14.28',
+//     '@types/redis': '^2.8.28',
+//     'dotenv': '^8.2.0',
+//     'ioredis': '^4.19.4',
+//     'redis': '^3.0.2',
+//     'redis-json': '^4.3.0',
+//     'redis-rejson': '^1.0.0',
+//     'tedis': '^0.1.12',
+//   },
+//   bundledDependencies: ['questrade-api-enumerations'],
+// };
