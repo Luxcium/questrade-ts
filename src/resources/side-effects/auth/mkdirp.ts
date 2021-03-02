76156fce4e7c4bf2f8571097bbb1e05d618c8182
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-03-02 18:18:48 -0500

    fix: dir creation process
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/resources/side-effects/auth/mkdirp.ts
+++ src/resources/side-effects/auth/mkdirp.ts
@@ -3,14 +3,14 @@ import { mkdir } from 'fs';
 import { resolve } from 'path';
 import { promisify } from 'util';
 
-import { void0 } from '../default-behaviour';
+// import { void0 } from '../default-behaviour';
 
 export async function mkDirP(p: string) {
   const myPath = resolve(p);
-  const newmask = 0o0777;
-  const mode = newmask;
-  const oldmask = process.umask(newmask);
-  void0(`Changed umask from ${oldmask.toString(8)} to ${newmask.toString(8)}`);
+  // const newmask = 0o0777;
+  // const mode = newmask;
+  // const oldmask = process.umask(newmask);
+  // void0(`Changed umask from ${oldmask.toString(8)} to ${newmask.toString(8)}`);
 
-  return promisify(mkdir)(myPath, { mode, recursive: true });
+  return promisify(mkdir)(myPath, { /* mode, */ recursive: true });
 }
