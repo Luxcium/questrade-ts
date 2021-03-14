8b57af931cb4a1cf44365d9c3b4b8bae91cf1215
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-03-14 06:27:19 -0400

    new assets and test image in readme
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/draft/debug/SCIENTIA_ES_LUX_PRINCIPIUM.ts
+++ src/draft/debug/SCIENTIA_ES_LUX_PRINCIPIUM.ts
@@ -8,29 +8,19 @@ import { mainRedis } from '../code/main-redis';
 import { getIdsAndSymbolsList } from '../code/will-get-symbol-id-and-first-symbol';
 import { saveValueToDB } from './saveValueToDB';
 
-export async function SCIENTIA_ES_LUX_PRINCIPIUM(dbCallQ: SimpleQueue) {
-  // activate the api for later use
-  // ** // ** // ** // ** // ** //
+export async function SCIENTIA_ES_LUX_PRINCIPIUM(dbCallCue: SimpleQueue) {
   const { qtApi } = await mainRedis();
-  // ** // ** // ** // ** // ** //
   const step0 = getEquitySymbList({ qtApi, symbolList: getSnP500List });
-  // ** // ** // ** // ** // ** //
   const step1: xIdsAndSymbList = getIdsAndSymbolsList({
     ...(await step0),
   });
 
-  // ** // ** // ** // ** // ** //
   const step2 = ({ symbolId }: { symbolId: number }) =>
     getSymbol({ qtApi, symbolId });
-  // ** // ** // ** // ** // ** //
 
   const step3 = await mapping({ list: step1, mapper: step2 });
-  // ** // ** // ** // ** // ** //
-  // ** // ** // ** // ** // ** //
   const step5 = async (stockSymbol: IStockSymbol) => {
-    // const config = { Model: StockSymbol, value: stockSymbol };
-
-    saveValueToDB(dbCallQ)({
+    saveValueToDB(dbCallCue)({
       Model: StockSymbol,
       value: stockSymbol,
     });
@@ -45,8 +35,9 @@ export async function SCIENTIA_ES_LUX_PRINCIPIUM(dbCallQ: SimpleQueue) {
 
   return step8;
 }
-// return getSymbolIDSearchAndStockSymbolDbSave(qtApi, apiCallQ, idsAndSymbList);
 
 const fnStep7 = (step5: any, step6: any) => step6.map(step5);
 const fnStep6 = async (step7: any) => Promise.all(step7);
 const fnStep8 = fnStep6;
+
+// return getSymbolIDSearchAndStockSymbolDbSave(qtApi, apiCallQ, idsAndSymbList);
