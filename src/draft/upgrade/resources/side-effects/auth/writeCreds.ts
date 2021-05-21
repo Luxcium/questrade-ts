5d02a9d6b6a118d99f3440993711ea544ddb24e6
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-05-20 20:21:26 -0400

    moving write-creds to own folder
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

---
+++ src/draft/upgrade/resources/side-effects/auth/writeCreds.ts
@@ -0,0 +1,28 @@
+import { writeFile } from 'fs/promises';
+
+import { ClientRequestConfig } from '../../../../../resources/side-effects/types';
+import type {
+  ApiOptions,
+  Credentials,
+  ICreds,
+} from '../../../../../typescript';
+import { preValidateToken } from '../../../../../utils';
+
+export async function writeCreds(
+  apiOptions: ApiOptions,
+  responseCreds: Promise<ICreds>,
+  conf: Promise<{ config: ClientRequestConfig; credentials: Credentials }>,
+) {
+  const { credentials } = await conf;
+  writeFile(
+    apiOptions.keyFile ||
+      `${apiOptions.keyDir}/${preValidateToken(apiOptions) ?? 'ERROR'}`,
+    (await responseCreds).refreshToken,
+    'utf8',
+  );
+
+  return {
+    ...credentials,
+    ...(await responseCreds),
+  };
+}
