08991eef4f381be9683ceaacfaae4d5a004d0d76
Luxcium <Luxcium@users.noreply.github.com>
2024-10-07 09:00:51 -0400

    upgrade eslint support

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// eslint.config.mjs
+
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+);
